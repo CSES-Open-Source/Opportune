bd3908709349983c855753aded9bcb970543acd4
widjaja <wwidjaja0@gmail.com>
2024-12-02 15:29:27 -0800

    lint

---
+++ frontend/src/api/applications.ts
@@ -0,0 +1,17 @@
+/**
+ * 
+What is the issue?
+A clear description of the task or feature to be implemented by the engineers.
+
+Create a new file applications.ts in frontend/src/api.
+Create Application.ts in src/types to store the types related to API calls.
+Implement API function calls for Application in the frontend. These functions are the endpoints in the frontend that interact directly with the backend endpoints.
+Additional context
+Add any other context or screenshots about the enhancement here.
+
+For more information about what to send to the backend in the body, query and param, look at the backend documentation
+Feel free to reference the users.ts API functions that I have implemented
+Note
+
+The JSON sent by the backend can only contain primitive types like strings, therefore the parser for company can be used to parse the dates for each applicationStatus.
+ */
