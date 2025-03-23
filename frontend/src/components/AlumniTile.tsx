8462c7b4ffcae3071a4b53ce1decd81421ed1767
widjaja <wwidjaja0@gmail.com>
2025-03-22 23:51:24 -0700

    prototyping the alumni connect page

--- frontend/src/components/AlumniTile.tsx
+++ frontend/src/components/AlumniTile.tsx
@@ -16,15 +16,24 @@ const AlumniTile: React.FC<AlumniTileProps> = ({ data }) => {
     // navigate(`/users/alumni/${data.}`);
   };
 
+  // Profile - Name
+  //          Class
+
+  // Company + Details
+
+  // Position
+  // Email
+  // Number
+
   return (
     <div
       onClick={handleTileClick}
-      className="bg-white border border-gray-300 rounded-md ps-10 pe-8 py-4 mt-4 mx-2 shadow-sm hover:bg-primary hover:bg-opacity-[0.02] hover:shadow-md transition cursor-pointer"
+      className="bg-white border border-gray-300 rounded-md mt-4 mx-2 shadow-sm hover:bg-primary hover:bg-opacity-[0.02] hover:shadow-md transition cursor-pointer"
     >
-      <div className="flex sm:items-start items-center space-x-4 w-full">
-        {/* Left: logo and company info */}
-        <div className="flex flex-col sm:flex-row items-start sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 w-full gap-3">
-          <div className="w-16 h-16 overflow-hidden rounded flex-shrink-0">
+      <div className="flex sm:items-start items-center space-x-4 w-full bg-slate-100">
+        {/* Left: logo and alumni info */}
+        <div className="flex flex-col sm:flex-row p-5 items-start sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 w-full gap-3">
+          <div className="w-16 h-16 overflow-hidden rounded-full flex-shrink-0 border-2 border-white shadow-lg">
             <img
               src={
                 data.profilePicture && data.profilePicture.trim() !== ""
@@ -37,33 +46,60 @@ const AlumniTile: React.FC<AlumniTileProps> = ({ data }) => {
           </div>
           <div className="flex-1">
             <h2 className="text-lg font-semibold text-gray-800">{data.name}</h2>
-            <p className="text-sm text-gray-500">{"placeholder"}</p>
-            <div className="mt-2 flex flex-wrap gap-2">
-              {data.email && (
-                <span className="bg-gray-100 text-gray-700 text-xs px-2 py-1 rounded-full">
-                  {data.email}
-                </span>
-              )}
-              {data.phoneNumber && (
-                <span className="bg-gray-100 text-gray-700 text-xs px-2 py-1 rounded-full">
-                  {data.phoneNumber}
-                </span>
-              )}
-            </div>
+            <p className="text-sm text-gray-500">{"position will go here"}</p>
           </div>
         </div>
         {/* right: visit website link */}
-        {data.linkedIn && (
-          <a
-            href={data.linkedIn}
-            onClick={(e) => e.stopPropagation()}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-500 hover:text-primary transition-colors"
-          >
-            <FaExternalLinkAlt className="w-4 h-4" />
-          </a>
-        )}
+        <div className="p-8">
+          {data.linkedIn && (
+            <a
+              href={data.linkedIn}
+              onClick={(e) => e.stopPropagation()}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-500 hover:text-primary transition-colors"
+            >
+              <FaExternalLinkAlt className="w-4 h-4" />
+            </a>
+          )}
+        </div>
+      </div>
+      <div>
+        <div className="flex">
+          {data.company?.logo && (
+            <div className="w-16 h-16 overflow-hidden rounded-full flex-shrink-0 border-2 border-white shadow-lg">
+              <img
+                src={data.company.logo}
+                alt={`${data.company.name} logo`}
+                className="object-contain w-full h-full"
+              />
+            </div>
+          )}
+          <div className="">
+            {data.company?.name && (
+              <div className="text-lg font-bold">{data.company.name}</div>
+            )}
+            <div className="">alumni position</div>
+            {data.company?.city && <div>{data.company.city}</div>}
+            {data.company?.employees && (
+              <div>{data.company.employees} employees</div>
+            )}
+          </div>
+        </div>
+        <div>
+          {data.email && (
+            <a
+              href={"mailto::" + data.email}
+              onClick={(e) => e.stopPropagation()}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-500 hover:text-primary transition-colors"
+            >
+              {data.email}
+            </a>
+          )}
+        </div>
+        <div>{data.phoneNumber && <p>{data.phoneNumber}</p>}</div>
       </div>
     </div>
   );
--- frontend/src/pages/Connect.tsx
+++ frontend/src/pages/Connect.tsx
@@ -3,7 +3,7 @@ import DataGrid from "../components/DataGrid";
 import AlumniTile from "../components/AlumniTile";
 import { getAlumni } from "../api/users";
 import { APIResult } from "../api/requests";
-import { Alumni } from "../types/User";
+import { Alumni, UserType } from "../types/User";
 import { PaginatedData } from "../types/PaginatedData";
 
 /**
@@ -15,6 +15,7 @@ import { PaginatedData } from "../types/PaginatedData";
  */
 
 const Connect = () => {
+  /*
   let alumni: PaginatedData<Alumni> = {
     page: 0,
     perPage: 10,
@@ -31,40 +32,128 @@ const Connect = () => {
       }
     },
   );
+  */
+  const sampleAlumni: Alumni[] = [
+    {
+      _id: "1",
+      email: "bill@gmail.com",
+      name: "Bill Newman",
+      profilePicture:
+        "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+      type: UserType.Alumni,
+      linkedIn: "https://linkedin.com/bill/",
+      phoneNumber: "(909) 101-2055",
+      company: {
+        _id: "1",
+        name: "Google",
+        city: "Mountain View",
+        state: "CA",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+        employees: "100k+",
+        industry: "Tech",
+        url: "https://www.google.com",
+      },
+      shareProfile: true,
+    },
+    {
+      _id: "1",
+      email: "bill@gmail.com",
+      name: "Bill Newman",
+      profilePicture:
+        "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+      type: UserType.Alumni,
+      linkedIn: "https://linkedin.com/bill/",
+      phoneNumber: "(909) 101-2055",
+      company: {
+        _id: "2",
+        name: "Apple",
+        city: "Cupertino",
+        state: "CA",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+        employees: "100k+",
+        industry: "Consumer Electronics",
+        url: "https://www.apple.com",
+      },
+      shareProfile: true,
+    },
+    {
+      _id: "1",
+      email: "bill@gmail.com",
+      name: "Bill Newman",
+      profilePicture:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+      type: UserType.Alumni,
+      linkedIn: "https://linkedin.com/bill/",
+      phoneNumber: "(909) 101-2055",
+      company: {
+        _id: "4",
+        name: "Amazon",
+        city: "Seattle",
+        state: "WA",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+        employees: "1.5M+",
+        industry: "E-commerce",
+        url: "https://www.amazon.com",
+      },
+      shareProfile: true,
+    },
+    {
+      _id: "1",
+      email: "bill@gmail.com",
+      name: "Bill Newman",
+      profilePicture:
+        "https://cdn.iconscout.com/icon/free/png-256/free-meta-logo-icon-download-in-svg-png-gif-file-formats--messenger-chatting-social-media-pack-logos-icons-5582655.png",
+      type: UserType.Alumni,
+      linkedIn: "https://linkedin.com/bill/",
+      phoneNumber: "(909) 101-2055",
+      company: {
+        _id: "5",
+        name: "Meta",
+        city: "Menlo Park",
+        state: "CA",
+        logo: "https://cdn.iconscout.com/icon/free/png-256/free-meta-logo-icon-download-in-svg-png-gif-file-formats--messenger-chatting-social-media-pack-logos-icons-5582655.png",
+        industry: "Social Media",
+        url: "https://about.meta.com",
+      },
+      shareProfile: true,
+    },
+  ];
 
   return (
-    <div>
-      <div>
-        <h1 className="text-3xl font-bold mb-6">Alumni Directory</h1>
-        <p>Connect with graduates from your university</p>
-      </div>
-      <div>
-        <h1 className="text-3xl font-bold mb-6">Find Alumni</h1>
-        <SearchBar
-          selections={[
-            {
-              label: "Industry",
-              options: [
-                "Tech",
-                "Finance",
-                "Healthcare",
-                "Retail",
-                "Software",
-                "Consumer Electronics",
-              ],
-            },
-          ]}
-          placeholder="Search by name, company, or industry"
-        />
-      </div>
-      <div>
-        <DataGrid
-          data={alumni.data}
-          usePagination={true}
-          TileComponent={AlumniTile}
-          cols={3}
-          maxRows={5}
-        />
+    <div className="min-h-screen">
+      <div className="max-w-5xl mx-auto px-4 py-8">
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold mb-6">Alumni Directory</h1>
+          <p>Connect with graduates from your university</p>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg p-8 ">
+          <h1 className="text-2xl font-bold mb-6">Find Alumni</h1>
+          <SearchBar
+            selections={[
+              {
+                label: "Industry",
+                options: [
+                  "Tech",
+                  "Finance",
+                  "Healthcare",
+                  "Retail",
+                  "Software",
+                  "Consumer Electronics",
+                ],
+              },
+            ]}
+            placeholder="Search by name, company, or industry"
+          />
+        </div>
+        <div className="">
+          <DataGrid
+            data={sampleAlumni}
+            usePagination={true}
+            TileComponent={AlumniTile}
+            cols={2}
+            maxRows={5}
+          />
+        </div>
       </div>
     </div>
   );
