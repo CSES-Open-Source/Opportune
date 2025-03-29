5a5ca7383bf434d024ff697be74dc7a91cac87cf
widjaja <wwidjaja0@gmail.com>
2025-03-29 17:46:48 -0400

    added Article model based on backend docs

---
+++ backend/src/models/Article.ts
@@ -0,0 +1,29 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const articleSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+type Article = InferSchemaType<typeof articleSchema>;
+
+export default model<Article>("Article", articleSchema);
