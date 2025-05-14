77104ce0e1420b8f0897129fa97104823a21bd5c
Ulises Salinas <usalinas595@gmail.com>
2025-05-14 10:35:58 -0700

    Added a button to create a new company

--- frontend/src/pages/Companies.tsx
+++ frontend/src/pages/Companies.tsx
@@ -72,13 +72,27 @@ const Companies: React.FC = () => {
               type="text"
               placeholder="Search companies..."
               className="
-                w-full border border-gray-300 rounded-full
+                w-3/4 border border-gray-300 rounded-full
                 py-2 pl-12 pr-4 text-sm
                 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
                 transition
               "
             />
           </div>
+          <button
+            onClick={() => setShowFilters(true)}
+            className="
+                      inline-flex items-center
+                      px-4 py-2
+                      bg-blue-500 text-white
+                      rounded-full
+                      hover:bg-blue-600
+                      focus:outline-none focus:ring-2 focus:ring-blue-300
+                      transition
+                      "
+          >
+            <span className="hidden sm:inline">Add New</span>
+          </button>
 
           {/* collapse to icon only on small, and shrink icon slightly */}
           <button
