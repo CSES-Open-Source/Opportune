91e5272bd0853c3a4222b515e4ffd22d7abdb98a
kevinsun0904 <kevinsun0904@gmail.com>
2025-05-19 00:16:29 -0700

    Added temp fix and disabled warnings

--- frontend/src/pages/Companies.tsx
+++ frontend/src/pages/Companies.tsx
@@ -20,17 +20,22 @@ const Companies: React.FC = () => {
     employees: "",
     industry: "",
   });
+
   // debounce search‐as‐you‐type (500ms)
   useEffect(() => {
     // TODO: Temporary solution to prevent refetching when the page is loaded.
-    if (initialized) {
-      const handle = setTimeout(() => {
-        setFilters((f) => ({ ...f, query }));
-      }, 500);
-      return () => clearTimeout(handle);
-    } else {
+    if (!initialized) {
       setInitialized(true);
+      return;
     }
+
+    const handle = setTimeout(() => {
+      setFilters((f) => ({ ...f, query }));
+    }, 500);
+    return () => clearTimeout(handle);
+
+    // Temporary fix
+    // eslint-disable-next-line
   }, [query]);
 
   // filter modal toggle
