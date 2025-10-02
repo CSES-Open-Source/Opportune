73b279c601673bcd118243c6e4dd9d1ace331dd9
n1sh1thaS <nishi2919@gmail.com>
2025-10-01 20:23:06 -0700

    migrate from cra to vite

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const tailwindConfig = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
